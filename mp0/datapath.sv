b1aa39040999c3698ef3d050b5fa5f2302672ddf
atsmith3 <atsmith3@illinois.edu>
2017-09-02 13:58:56 -0500

    Finished connecting datapath

--- mp0/datapath.sv
+++ mp0/datapath.sv
@@ -2,26 +2,57 @@ import lc3b_types::*;
 
 module datapath
 (
-    input clk,
-
     /* control signals */
-	 input storemux_sel,
-    input pcmux_sel,
-    input load_pc
+    input logic storemux_sel,
+    input logic pcmux_sel,
+    input logic load_pc,
+    input logic load_ir,
+    input logic load_regfile,
+    input logic load_mar,
+    input logic load_cc,
+    input logic pcmux_sel,
+    input logic storemux_sel,
+    input logic alumux_sel,
+    input logic regfilemux_sel,
+    input logic marmux_sel,
+    input logic mdrmux_sel,
+    input lc3b_aluop aluop,
 
-    /* declare more ports here */
+    /* Input Ports */
+    input logic clk,
+    lc3b_word mem_rdata,
+    
+    /* Output Ports */
+    output lc3b_word   mem_address,
+    output lc3b_word   mem_wdata,
+    output logic       branch_enable,
+    output lc3b_opcode opcode,
 );
 
 /* declare internal signals */
+lc3b_reg sr1;
+lc3b_reg sr2;
+lc3b_reg dest; 
+lc3b_reg storemux_out;
+lc3b_word sr1_out;
+lc3b_word sr2_out;
+
+lc3b_offset6 offset6;
+lc3b_offset9 offset9;
+lc3b_word adj6_out;
+lc3b_word adj9_out;
 lc3b_word pcmux_out;
+lc3b_word alumux_out;
+lc3b_word regfilemux_out;
+lc3b_word marmux_out;
+lc3b_word mdrmux_out;
+lc3b_word alu_out;
 lc3b_word pc_out;
 lc3b_word br_add_out;
 lc3b_word pc_plus2_out;
-lc3b_word ir_out;
+lc3b_nzp  gencc;
+lc3b_nzp  cc_out;
 
-lc3b_reg sr1;
-lc3b_reg dest;
-lc3b_reg storemux_out;
 
 /*
  * PC
@@ -42,15 +73,159 @@ register pc
     .out(pc_out)
 );
 
+always_comb
+begin : branch_add
+    br_add_out = pc_out + adj9_out;
+end
+
+always_comb
+begin : pc_plus_two
+    pc_plus2_out = pc_out + 2;
+end
+
+
+/*
+ * IR
+ */
+ir _ir
+(
+    .clk(clk),
+    .load(load_ir),
+    .in(mem_wdata),
+    .opcode(opcode),
+    .dest(dest),
+    .src1(src1),
+    .src2(src2),
+    .offset6(offset6),
+    .offset9(offset9)
+);
+
+
+/*
+ * ADJ
+ */
+adj #(.width(6)) _adj6
+(
+    .in(offset6),
+    .out(adj6_out)
+);
+adj #(.width(9)) _adj9
+(
+    .in(offset9),
+    .out(adj9_out)
+);
+
+
+/*
+ * REG FILE
+ */
+module regfile
+(
+    .clk(clk),
+    .load(load_regfile),
+    .in(regfilemux_out),
+    .src_a(sr1),
+    .src_b(sr2),
+    .dest(dest),
+    .reg_a(sr1_out),
+    .reg_b(sr2_out)
+);
+
+
 /*
  * StoreMux
  */
 mux2 #(.width(3)) storemux 
 (
     .sel(storemux_sel),
-	 .a(sr1),
-	 .b(dest),
-	 .f(storemux_out)
+    .a(sr1),
+    .b(dest),
+    .f(storemux_out)
 );
+
+
+/*
+ * MAR
+ */ 
+mux2 marmux
+(
+    .sel(marmux_sel),
+    .a(alu_out),
+    .b(pc_out),
+    .f(marmux_out)
+);
+register mar
+(
+    .clk(clk),
+    .load(load_mar),
+    .in(marmux_out),
+    .out(mem_address)
+);
+
+/*
+ * MDR
+ */
+mux2 mdrmux
+(
+    .sel(mdrmux_sel),
+    .a(alu_out),
+    .b(mem_rdata),
+    .f(mdrmux_out)
+);
+register mdr
+(
+    .clk(clk),
+    .load(load_mdr),
+    .in(mdrmux_out),
+    .out(mem_wdata)
+);
+
+
+/*
+ * ALU
+ */
+alu _alu
+(
+    .aluop(aluop),
+    .a(sr1_out),
+    .b(alumux_out),
+    .f(alu_out)
+);
+mux2 alumux
+(
+    .sel(alumux_sel),
+    .a(sr2_out),
+    .b(adj6_out),
+    .f(alumux_out)
+);
+
+
+/*
+ * CC
+ */
+gencc _gencc
+(
+    .in(regfilemux_out),
+    .out(gencc_out)
+);
+register cc
+(
+    .clk(clk),
+    .load(load_cc),
+    .in(gencc_out),
+    .out(cc_out)
+);
+mux2 regfilemux
+(
+    .sel(regfilemux_sel),
+    .a(alu_out),
+    .b(mem_wdata),
+    .f(regfilemux_out)
+);
+always_comb
+begin : CCCOMP
+    if(cc_out == dest) branch_enable = 1;
+    else branch_enable = 0;
+end
 
 endmodule : datapath
